491b257c6043a023a2645123aeb9a71f56cf2d3c
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-04-30 17:23:25 +0100

    Stored input value

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
 
 function App() {
+  function handleInputValue(input) {
+    console.log(input.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
       <header>Recipe Generator</header>
-      <form>
-        <input type="search" placeholder="Search for a recipe" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Search for a recipe"
+          onChange={handleInputValue}
+        />
         <input type="submit" value="Search" />
       </form>
 
