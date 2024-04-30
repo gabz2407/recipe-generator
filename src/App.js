51b4d6d0de903a68b346c4d2e1b7310502b2d539
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-04-30 17:31:57 +0100

    Added API response

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import React, { useState } from "react";
+import axios from "axios";
+
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data.answer);
+  }
+
   function handleInputValue(input) {
     console.log(input.target.value);
+    setInput(input.target.value);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
+    let prompt = `Generate a recipe about ${input}`;
+    let context = `Accordingly to your knowledge about world foods, generate a healthy recipe`;
+    let apiKey = "8bc029ce07bb99a925obf42d966t543f";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   return (
