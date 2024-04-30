1df688f0825fbf32b55b713dc6c9d4bcd35c139d
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-04-30 17:13:40 +0100

    project setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header>Recipe Generator</header>
+      {/* form */}
+
+      {/* option buttons
+      - vegan
+      - vegetarian
+      - carnivore
+      - pescatarian */}
     </div>
   );
 }
