983eb3e982416b4740cae7c2527b8f89e3d39e83
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-04-30 18:27:49 +0100

    Changed html response

--- src/App.js
+++ src/App.js
@@ -5,21 +5,36 @@ import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
+  const [answer, setAnswer] = useState("");
 
   function handleResponse(response) {
     console.log(response.data.answer);
+    setAnswer(response.data.answer);
   }
 
   function handleInputValue(input) {
-    console.log(input.target.value);
     setInput(input.target.value);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
     let prompt = `Generate a recipe about ${input}`;
-    let context = `Accordingly to your knowledge about world foods, generate a healthy recipe`;
-    let apiKey = "8bc029ce07bb99a925obf42d966t543f";
+    let context = `Accordingly to your knowledge about world foods, generate a healthy recipe like this: 
+    <h1>Recipe Title</h1> 
+    <br /> 
+    <br /> 
+    <h3>Ingredients:</h3>
+    <ul>
+    <li> ingredients list </li>
+    </ul>
+    <br />
+    <br />
+    <h3>Instructions:</h3>
+    <ul>
+    <li>Instructions list </li>
+    </ul>
+    `;
+    let apiKey = "ad4a76bd16e24537a83abaf57d8dcaa3";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     axios.get(apiUrl).then(handleResponse);
@@ -36,7 +51,7 @@ function App() {
         />
         <input type="submit" value="Search" />
       </form>
-
+      <div>{answer}</div>
       {/* option buttons
       - vegan
       - vegetarian
